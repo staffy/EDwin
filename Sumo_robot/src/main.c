acbecce1e0bf704ca66db2f501e443f03ba15551
unknown <Emelie@Emelie-Dator.(none)>
2012-04-26 18:38:22 +0200

    reflex sensors work fine

--- Sumo_robot/src/main.c
+++ Sumo_robot/src/main.c
@@ -45,105 +45,108 @@ __CRP const unsigned int CRP_WORD = CRP_NO_CRP ;
 #define KILL_SWITCH_BIT 3	// Pin on port for kill_switch
 
 
-int main(void) {
-	//GPIOInit();
-		SysTick_Config( SystemCoreClock/1000 );
-		//SysTick->CTRL &= (0 << 1); 	// Need to be commected for lcd
-		initDrive();
-		ADCInit( ADC_CLK );
-		//LCD_init();
-		//initReflex();
-		//GPIOSetDir( 3, 2, 1 );
-		//GPIOSetValue( 3, 2, 0);
-		GPIOSetDir( LED_PORT, LED_BIT, 1 );
-		GPIOSetValue( LED_PORT, LED_BIT, LED_ON );
-		GPIOSetDir( 3, 2, 1 );
-		// Enter an infinite loop, just incrementing a counter
-		//LCD_home();
-		//LCD_clear();
-		//LCD_sendStr("Init done!");
-		while(1)
-		{
-			GPIOSetValue( LED_PORT, LED_BIT, LED_ON );
+int main(void)
+{
+	GPIOInit();
+	//SysTick_Config( SystemCoreClock/1000 );
+	//SysTick->CTRL &= (0 << 1); 	// Need to be commected for lcd
+	initDrive();
+	//ADCInit( ADC_CLK );
+	//LCD_init();
+	//initReflex();
+	//GPIOSetDir( 3, 2, 1 );
+	//GPIOSetValue( 3, 2, 0);
+	GPIOSetDir( LED_PORT, LED_BIT, 1 );
+	GPIOSetValue( LED_PORT, LED_BIT, LED_OFF );
+	GPIOSetDir( 3, 2, 1 );
+	GPIOSetDir( START_PORT, START_BIT, 0);
+	GPIOSetDir( KILL_SWITCH_PORT, KILL_SWITCH_BIT, 0);
+	// Enter an infinite loop, just incrementing a counter
+	//LCD_home();
+	//LCD_clear();
+	//LCD_sendStr("Init done!");
+
+	init_timer32(0, TIME_INTERVAL);
+	enable_timer32(0);
+
+	while(1)
+	{
+			//GPIOSetValue( LED_PORT, LED_BIT, LED_OFF );
 			//uint16_t adcValue = ADCRead( 5 );
-			if(reflexRead())//adcValue > 100)
+			/*if(reflexRead())//adcValue > 100)
 			{
 				GPIOSetValue( LED_PORT, LED_BIT, LED_OFF );
 			}
 			else
 			{
 				GPIOSetValue( LED_PORT, LED_BIT, LED_ON );
-			}
+			}*/
 
 
-
-			//---MAIN---//
+		//-----MAIN-----//
 //Start signal: 0, Kill switch: 1, LED: off -> POWER ON
-			while(!GPIOReadValue(START_PORT, START_BIT) && GPIOReadValue(KILL_SWITCH_PORT, KILL_SWITCH_BIT))//while not start signal and kill switch signal
-			{
-				//LED = släckt
-				GPIOSetValue( LED_PORT, LED_BIT, LED_OFF );
-				//do nothing
-			}
+		while(!GPIOReadValue(START_PORT, START_BIT) && GPIOReadValue(KILL_SWITCH_PORT, KILL_SWITCH_BIT))//while not start signal and kill switch signal
+		{
+			//LED = släckt
+			GPIOSetValue( LED_PORT, LED_BIT, LED_OFF );
+			//do nothing
+		}
 
 
 //Start signal: 1, Kill switch: 1, LED: on -> STARTED
 			//enable_engines();//gör nåt annat smart!
-			while(GPIOReadValue(START_PORT, START_BIT) && GPIOReadValue(KILL_SWITCH_PORT, KILL_SWITCH_BIT))//while not stop
+		while(GPIOReadValue(START_PORT, START_BIT) && GPIOReadValue(KILL_SWITCH_PORT, KILL_SWITCH_BIT))//while not stop
+		{
+			//LED = TÄND
+			GPIOSetValue( LED_PORT, LED_BIT, LED_ON );
+	/*		switch(reflexRead())
 			{
-				//LED = TÄND
-				GPIOSetValue( LED_PORT, LED_BIT, LED_ON );
-		/*		switch(reflexRead())
-				{
-					case 0:
-						set_movement(1, 60);//backwards
-						break;
-					case 1:
-						set_movement(1, 60);//backwards
-						break;
-					case 2:
+				case 0:
+					set_movement(1, 60);//backwards
+					break;
+				case 1:
+					set_movement(1, 60);//backwards
+					break;
+				case 2:
+					set_movement(0, 60);//forward
+					break;
+				case 3:
+					set_movement(0, 60);//forward
+					break;
+				case 4:
+					set_movement(2, 60);//turn right
+					//if object found in front
+					if(1)//if(sharpRead() == 4 || sharpRead == 5)
+					{
 						set_movement(0, 60);//forward
-						break;
-					case 3:
-						set_movement(0, 60);//forward
-						break;
-					case 4:
-						set_movement(2, 60);//turn right
-						//if object found in front
-						if(1)//if(sharpRead() == 4 || sharpRead == 5)
-						{
-							set_movement(0, 60);//forward
-						}
-						//if object found behind
-						if(2)//if(sharpRead() == 1 || sharpRead() == 2)
-						{
-							set_movement(1, 60);//backwards
-						}
-						break;
-					default:
-						break;
-					}*/
-			}
+					}
+					//if object found behind
+					if(2)//if(sharpRead() == 1 || sharpRead() == 2)
+					{
+						set_movement(1, 60);//backwards
+					}
+					break;
+				default:
+					break;
+				}*/
+		}
+
 //Start signal: 0, Kill switch: 0, LED: flashing -> STOPPED
-			while(!GPIOReadValue(START_PORT, START_BIT) && !GPIOReadValue(KILL_SWITCH_PORT, KILL_SWITCH_BIT))
-			{
-				if(reflexRead() == 0)
+		while(!GPIOReadValue(START_PORT, START_BIT) && !GPIOReadValue(KILL_SWITCH_PORT, KILL_SWITCH_BIT))
+		{
+				if ( (timer32_0_counter%LED_TOGGLE_TICKS) < (LED_TOGGLE_TICKS/2) )
 				{
-					if ( (timer32_0_counter%LED_TOGGLE_TICKS) < (LED_TOGGLE_TICKS/2) )
-					{
-					  GPIOSetValue( LED_PORT, LED_BIT, LED_OFF );
-					}
-					else
-					{
-					  GPIOSetValue( LED_PORT, LED_BIT, LED_ON );
-					}
-					/* Go to sleep to save power between timer interrupts */
-					__WFI();
+					GPIOSetValue( LED_PORT, LED_BIT, LED_OFF );
 				}
-				GPIOSetValue( LED_PORT, LED_BIT, LED_OFF );
+				else
+				{
+	  			  GPIOSetValue( LED_PORT, LED_BIT, LED_ON );
+				}
+				/* Go to sleep to save power between timer interrupts */
+				__WFI();
 
 				//disable_engines();
-			}
 		}
-		return 0 ;
+	}
+	return 0 ;
 }
